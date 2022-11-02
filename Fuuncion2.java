72e3b9cf417f012be13728906103102753db7c76
Jossie7 <jyovcon215@g.educaand.es>
2022-11-02 09:29:14 +0100

    Create Fuuncion2.java
    es el else if

---
+++ Fuuncion2.java
@@ -0,0 +1,25 @@
+public class Fuuncion2 {
+    public static void main(String[] args){
+        int dia=1;
+        
+        //No es necesario pooner las llaves para colocar la función,ya que el if busca hasta su punto y coma.
+        if(dia==1)
+            System.out.println("Hoy es Lunes ");
+        
+        else if(dia==2)
+            System.out.println("Hoy es martes");
+        
+        else if(dia==3)
+            System.out.println("Hoy es miércoles");
+        
+        else if(dia==4)
+            System.out.println("Hoy es jueves");
+        
+        else if(dia==5)
+            System.out.println("Hoy es viernes");
+        
+        else
+            System.out.println("Hoy es fin de semana");
+        
+    }
+}
