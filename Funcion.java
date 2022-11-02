bc2e5c380833b62a31fecde7cad105f5ba8b03cb
Jossie7 <jyovcon215@g.educaand.es>
2022-11-02 09:33:14 +0100

    Create Funcion.java
    Es la función if

---
+++ Funcion.java
@@ -0,0 +1,17 @@
+public class Funcion {
+    public static void main (String[] args){
+        // Declaramos variables
+        int a=1;
+        int b=2;
+        //Usamos la funcion de si es que es mayor
+        if (a>b){
+            System.out.println(a + " es el valor de A y  es mayor que B");
+        }
+        else if(b>a) {
+            System.out.println(b + " es el valor de B y  es mayor que A");
+        }
+        else {
+            System.out.println( " A y B son iguales");  
+        }
+    }
+}
